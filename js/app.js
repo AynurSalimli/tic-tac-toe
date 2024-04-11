1e372332afcc4bf4a3334de99862c22c15f7e0ae
AynurSalimli <aynur.selimli@ascca.edu.az>
2024-04-11 15:39:03 +0400

    progress

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelector(".items");
+
+
+const App = {
+    //All of our selected HTML elements
+$:{
+
+}
+}
+menu.addEventListener("click", (event) => {
+  menuItems.classList.toggle("hidden");
+});
